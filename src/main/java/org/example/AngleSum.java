90c11e762355d8f8cf0ed3c7a98b04aa30762b57
Vitalii Voitseshuk <voitseshuk.v@gmail.com>
2024-01-08 15:03:31 +0200

    add AngleSum implementation

---
+++ src/main/java/org/example/AngleSum.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class AngleSum {
+  public static int sumOfAngles(int n) {
+    return (n - 2) * 180;
+  }
+}
---
+++ src/test/java/org/example/AngleSumTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class AngleSumTest {
+    @Test
+    public void sampleTests() {
+        assertEquals(180, AngleSum.sumOfAngles(3));
+        assertEquals(360, AngleSum.sumOfAngles(4));
+    }
+}
